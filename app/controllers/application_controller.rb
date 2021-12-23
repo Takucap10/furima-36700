76adbba5149e3f5e9bd209b823e333f9621a2037
Takucap10 <t.okusako10@gmail.com>
2021-12-23 10:56:48 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configue_permitted_parameters, if: devise_controller?
 
+  private
+  def configue_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_kana, :last_name_kana, :date])
+  end
 
 end
