478d166c08243f493478038618ebd7c7e4ff982b
Takucap10 <t.okusako10@gmail.com>
2022-01-04 10:36:27 +0900

    ActiveHash 導入　参照ファイル生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtentions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :sending_day
 end
