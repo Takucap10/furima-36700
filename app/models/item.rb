44cb009c19426a823ce0d0207df5d1aa46cffaf5
Takucap10 <t.okusako10@gmail.com>
2022-01-05 22:34:10 +0900

    ActiveHash　バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,15 @@ class Item < ApplicationRecord
   belongs_to :sending_day
 
   belongs_to :user
+
+  with_options presence: true do
+  validates :name
+  validates :information
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sending_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price
+  end
 end
