481ccc546cb65dec7ca2027af8de0df6f9153fba
Takucap10 <t.okusako10@gmail.com>
2022-01-05 23:02:34 +0900

    コントローラー　ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit:(:name,:information, :category_id, :status_id, :postage_id, :prefecture_id, :sending_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
