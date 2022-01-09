3be8fdfc905b69eed4abb6709f54733b76daa1dc
Takucap10 <t.okusako10@gmail.com>
2022-01-09 12:14:57 +0900

    商品画像バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,6 +21,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sending_day_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
+  validates :image
   end
   validates :price, numericality: { greater_than_or_equal_to: 300 , less_than_or_equal_to: 9999999 , message: 'is out of setting range'}
 end
