ebbfe310f88daac6c35d13a266d9981c47c0e3d1
Takucap10 <t.okusako10@gmail.com>
2022-01-10 22:20:14 +0900

    rubocop 作動

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :sending_day_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-      message: 'is out of setting range' }
+                                      message: 'is out of setting range' }
     validates :image
   end
 end
