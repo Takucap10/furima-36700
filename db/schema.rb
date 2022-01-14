421b9aaf8911c812dd55d4ae2e5142ba231d929f
Takucap10 <t.okusako10@gmail.com>
2022-01-14 20:48:45 +0900

    sendingAddressテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_114550) do
+ActiveRecord::Schema.define(version: 2022_01_14_114733) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 2022_01_14_114550) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "sending_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "sending_log_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sending_log_id"], name: "index_sending_addresses_on_sending_log_id"
+  end
+
   create_table "sending_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
@@ -77,6 +90,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_114550) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "sending_addresses", "sending_logs"
   add_foreign_key "sending_logs", "items"
   add_foreign_key "sending_logs", "users"
 end
