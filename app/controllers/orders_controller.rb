360fdc89afcf94f0bdfc39ea78c46bb76c2953f0
Takucap10 <t.okusako10@gmail.com>
2022-01-15 21:25:18 +0900

    Formオブジェクトの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,23 +2,25 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
   
   def new
+    @order_address = OrderAddress.new
   end
   
   def create
-    @order = Order.create(order_params)
-    Address.create(address_params)
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
     redirect_to root_path
+    else
+      render :index
+    end
   end
   
   private
   def order_params
     params.merge(user_id: current_user.id).merge(item_id: @item.id)
-  end
-  
-  def address_params
-    params.permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge( order_id: @order.id)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,12 +3,15 @@ class OrderAddress
   attr_accessor :post_code, :prefecture_id, :city, :block, :building, :phone_number
 
   with_options presence: true do 
-    varidates :user_id
-    validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(123-4567)"}
     validates :city
     validates :block
     validates :phone_number, numericality: {only_integer: true, message: "is invalid. Input only number"}, length:{ minimum: 10, message: "is too short (minimum length 10)"}
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Adress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
