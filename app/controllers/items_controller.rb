3f9242c2cbb72c1a6e38ca28d1fba585780e43fa
Takucap10 <t.okusako10@gmail.com>
2022-01-16 20:19:25 +0900

    index　販売後の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order('created_at DESC')
+    @orders = Order.all
   end
 
   def new
@@ -21,7 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @order = Order.all
+    @orders = Order.all
   end
 
   def edit
