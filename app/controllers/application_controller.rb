e961661bd5779e4213b3e2dafd943b6ab7cbf021
Takucap10 <t.okusako10@gmail.com>
2022-01-23 17:01:40 +0900

    最終提出手前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,3 +13,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day])
   end
+end
